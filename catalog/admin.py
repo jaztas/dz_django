31eab1bdb99fe4b778fcb5595db68d74b2a9a5b4
Zotov Stanislav <zotovstanislav@mail.ru>
2024-05-22 20:24:36 +0300

    модели добавлены в админку для отображения

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from catalog.models import Category, Product
+
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_filter = ('id',)
+    search_fields = ('name', 'description',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
