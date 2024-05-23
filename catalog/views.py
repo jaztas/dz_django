883590ec5e03e3244eba9fb8cd7089fcf32fbb25
Zotov Stanislav <zotovstanislav@mail.ru>
2024-05-24 02:17:31 +0300

    добавил контроллеры для товаров

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
+
 
 def home(request):
 	return render(request, 'catalog/home.html')
+
 
 def contacts(request):
 	if request.method == 'POST':
@@ -10,3 +14,19 @@ def contacts(request):
 		message = request.POST.get['message']
 		return render(request, 'catalog/contacts.html', {'name': name, 'email': email, 'message': message})
 	return render(request, 'catalog/contacts.html')
+
+
+def products(request):
+	product = Product.objects.all
+	context = {
+		'object_list': product,
+		'title': 'Товары'
+	}
+	return render(request, 'catalog/products.html', context=context)
+
+
+def product(request, pk):
+	context = {
+		'object_list': Product.objects.get(pk=pk)
+	}
+	return render(request, 'catalog/product.html', context=context)
