9446402e88b0d0bc41c7c78f5d6cc3b6ce2186d7
Zotov Stanislav <zotovstanislav@mail.ru>
2024-05-24 02:19:49 +0300

    добавил урлы

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from catalog.views import home
+from catalog.views import home, contacts, product, products
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-	path('', home)
+    path('', home),
+    path('contacts/', contacts, name='contacts'),
+    path('products/', products, name='products'),
+    path('<int:pk>', product, name='product'),
 ]
